715578b8bd445971b56f5d6e17b058d1377016ce
itai rozen <itai.rozen@mail.huji.ac.il>
2024-10-23 01:38:08 +0300

    update litterbox status on click

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-var getCatEmoji = function () {
-    var emojies = ['', '', ''];
-    var now = new Date();
-    console.log('now: ', now);
-    var lastSandReplace = now.setDate(1);
-    var lastLitterCleaning = now.setDate(2);
-    console.log('last replace: ', lastSandReplace);
-    console.log('last litter cleaning: ', lastLitterCleaning);
-    return null;
+var lastSandReplaceDate = new Date('2024-06-15');
+var lastLitterCleanDate = new Date('2024-06-15');
+var setDateText = function (type, date) {
+    console.log('date: ', date);
+    var textEl = document.getElementById("last-".concat(type));
+    textEl.innerText = date.toLocaleTimeString('en-GB', {
+        year: '2-digit',
+        month: 'numeric',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: false // for 24h format
+    });
 };
 var updateCatEmoji = function (status) {
     var emojies = ['😿', '🐱', '😺'];
-    document.getElementById('cat-emoji').innerHTML = emojies[status];
+    document.getElementById('cat-emoji').innerText = emojies[status];
+};
+var buttonClicked = function (buttonType) {
+    setDateText(buttonType, new Date(Date.now()));
 };
 var onInit = function () {
+    console.log('init!');
     updateCatEmoji(1);
+    setDateText('clean', lastLitterCleanDate);
+    setDateText('replace', lastSandReplaceDate);
 };
--- script.ts
+++ script.ts
@@ -1,20 +1,31 @@
-const getCatEmoji = () : null => {
-  const emojies : string[] = ['', '', '']
-  const now : Date = new Date();
-  console.log('now: ', now)
-  const lastSandReplace : number = now.setDate(1)
-  const lastLitterCleaning : number = now.setDate(2)
-  console.log('last replace: ', lastSandReplace)
-  console.log('last litter cleaning: ', lastLitterCleaning)
-  return null
+let lastSandReplaceDate : Date = new Date('2024-06-15')
+let lastLitterCleanDate : Date = new Date('2024-06-15')
+
+const setDateText : Function = (type : string, date: Date) => {
+  console.log('date: ', date)
+  const textEl = document.getElementById(`last-${type}`) as HTMLElement
+  textEl.innerText = date.toLocaleTimeString('en-GB', {
+    year: '2-digit',
+    month: 'numeric',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: false  // for 24h format
+  })
 }
 
 const updateCatEmoji = (status:number) => {
   const emojies: string[] = ['😿', '🐱', '😺']
-  document.getElementById('cat-emoji')!.innerHTML = emojies[status]
+  document.getElementById('cat-emoji')!.innerText = emojies[status]
 }
 
+const buttonClicked: Function = (buttonType: string) => {
+  setDateText(buttonType, new Date(Date.now()))
+}
 const onInit: Function = () => {
+  console.log('init!')
   updateCatEmoji(1)
+  setDateText('clean', lastLitterCleanDate)
+  setDateText('replace', lastSandReplaceDate)
 }
 
